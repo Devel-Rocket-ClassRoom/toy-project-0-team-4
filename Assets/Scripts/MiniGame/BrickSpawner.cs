1d4dd1e53e2e05f319064e92357928841d0cd0e8
이현진 <zse846@gmail.com>
2026-04-30 17:35:20 +0900

    벽돌뒤에 버튼 생성
    벽돌뒤에 버튼 생성해서 벽돌을 깨고 클릭하는 기능 구현

--- Assets/Scripts/MiniGame/BrickSpawner.cs
+++ Assets/Scripts/MiniGame/BrickSpawner.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class BrickSpawner : MonoBehaviour
 {
-    [Header("Settings")]
-    public GameObject brickPrefab;    
+    public GameObject brickPrefab;
+    public GameObject agreeButtonPrefab;
     public Transform brickContainer;
+
+    private List<GameObject> spawnedBricks = new List<GameObject>();
 
     void Start()
     {
-        // 게임 시작 시 6x2 벽돌 생성 실행
         SpawnBricks(6, 2);
     }
 
+    // 벽돌을 columns * rows 형태로 생성
     public void SpawnBricks(int columns, int rows)
     {
         int totalBricks = columns * rows;
@@ -20,6 +23,7 @@ public class BrickSpawner : MonoBehaviour
         for (int i = 0; i < totalBricks; i++)
         {
             GameObject newBrick = Instantiate(brickPrefab, brickContainer);
+            spawnedBricks.Add(newBrick);
         }
 
         GridLayoutGroup grid = brickContainer.GetComponent<GridLayoutGroup>();
@@ -30,5 +34,22 @@ public class BrickSpawner : MonoBehaviour
             Canvas.ForceUpdateCanvases();
             grid.enabled = false;
         }
+
+        PlaceButtonAtRandomBrick();
+    }
+
+    private void PlaceButtonAtRandomBrick()
+    {
+        if (spawnedBricks.Count == 0) return;
+
+        int randomIndex = Random.Range(0, spawnedBricks.Count);
+        GameObject targetBrick = spawnedBricks[randomIndex];
+
+        GameObject btn = Instantiate(agreeButtonPrefab, brickContainer);
+
+        // 버튼을 계층 구조의 맨 위로 보내서 벽돌 뒤에 숨김
+        btn.transform.SetAsFirstSibling();
+
+        btn.transform.localPosition = targetBrick.transform.localPosition;
     }
 }
