f2e9e905a5663ab18eaa9536c937ddf278f00e95
kimjunyoung <suk558165@gmail.com>
2026-05-01 20:14:38 +0900

    확인용 코드들 제거

--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -5,19 +5,11 @@ public class MiniGameManager : MonoBehaviour
 {
     public static MiniGameManager Instance { get; private set; }
 
-    // 각 미니게임 시작 이벤트
     public static event Action OnGame1Start;
     public static event Action OnGame2Start;
     public static event Action OnGame3Start;
-    // 미니게임 종료 이벤트 (성공/실패)
     public static event Action OnMiniGameSuccess;
     public static event Action OnMiniGameFail;
-
-    // 전체 게임 클리어
-    public static event Action OnAllGamesCleared;
-
-    private int currentGameIndex = 0;
-    private readonly int totalGames = 3;
 
     void Awake()
     {
@@ -25,33 +17,8 @@ public class MiniGameManager : MonoBehaviour
         Instance = this;
     }
 
-    void Start()
-    {
-        OnMiniGameSuccess += HandleSuccess;
-        OnMiniGameFail += HandleFail;
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            int random = UnityEngine.Random.Range(1, 4); // 1, 2, 3 중 랜덤
-            Debug.Log($"[Test] 스페이스바 → 미니게임 {random} 호출");
-            StartGame(random);
-        }
-    }
-
-    void OnDestroy()
-    {
-        OnMiniGameSuccess -= HandleSuccess;
-        OnMiniGameFail -= HandleFail;
-    }
-
     public void StartGame(int index)
     {
-        currentGameIndex = index;
-        Debug.Log($"[MiniGameManager] 미니게임 {index} 시작");
-
         switch (index)
         {
             case 1: OnGame1Start?.Invoke(); break;
@@ -60,17 +27,6 @@ public class MiniGameManager : MonoBehaviour
         }
     }
 
-    // 외부에서 성공/실패 알릴 때 사용
     public static void NotifySuccess() => OnMiniGameSuccess?.Invoke();
     public static void NotifyFail()    => OnMiniGameFail?.Invoke();
-
-    private void HandleSuccess()
-    {
-        Debug.Log($"[MiniGameManager] 게임 {currentGameIndex} 성공");
-    }
-
-    private void HandleFail()
-    {
-        Debug.Log($"[MiniGameManager] 게임 {currentGameIndex} 실패");
-    }
 }
