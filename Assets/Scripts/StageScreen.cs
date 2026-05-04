69e8448ecfdc283757b1e171f1df4fe47f1d33b1
박우빈 <oi3oi3oi@naver.com>
2026-05-04 10:18:24 +0900

    스테이지 관리 개선을 위한 리팩터 메인스크린UI 및 온클릭 버튼
    - CheckButton을 startButton으로, clearIcon을 clearPrefab으로 대체하도록 MainScreenUI를 업데이트했습니다.
    
    - 스테이지가 클리어되면 클리어프레팹을 생성하고 스테이지가 클리어되지 않으면 파괴하는 기능이 추가되었습니다.
    
    - 제목 화면과 메인 화면 간의 전환을 간소화하고 단일 방법으로 단계를 시작할 수 있도록 OnClickButton을 리팩토링했습니다.
    - 미니게임스파너를 도입하여 스테이지 클리어 및 게임 오버 이벤트를 포함한 미니게임 인스턴스화 및 관리를 처리합니다.
    - 더 나은 성능을 위해 PlayerPrefs 대신 클리어된 스테이지의 인메모리 추적을 사용하도록 StageClearManager를 수정했습니다.
    - 향상된 StageScreen은 게임 오버 시나리오를 처리하고 스테이지 논리를 더 효과적으로 정리합니다.

--- Assets/Scripts/StageScreen.cs
+++ Assets/Scripts/StageScreen.cs
@@ -4,28 +4,63 @@ using UnityEngine.UI;
 
 public class StageScreen : MonoBehaviour
 {
-    [Header("스테이지 정보")]
-    [SerializeField] private int stageNumber;
-
     [Header("버튼")]
     [SerializeField] private Button clearButton;
 
     public event Action<int> OnStageClearButtonClicked;
+    public event Action OnGameOver;
+
+    private int stageNumber;
+    private bool isFinished;
 
     private void Awake()
     {
-        clearButton.onClick.AddListener(OnClickClearButton);
+        if (clearButton != null)
+        {
+            clearButton.onClick.AddListener(OnClickClearButton);
+        }
     }
 
     private void OnDestroy()
     {
-        clearButton.onClick.RemoveListener(OnClickClearButton);
+        if (clearButton != null)
+        {
+            clearButton.onClick.RemoveListener(OnClickClearButton);
+        }
+    }
+
+    public void Init(int stageNumber)
+    {
+        this.stageNumber = stageNumber;
+        isFinished = false;
     }
 
     private void OnClickClearButton()
     {
-        Debug.Log($"클리어 버튼 눌림");
+        ClearStage();
+    }
+
+    public void ClearStage()
+    {
+        if (isFinished)
+            return;
+
+        isFinished = true;
+
+        Debug.Log($"{stageNumber} 스테이지 클리어");
 
         OnStageClearButtonClicked?.Invoke(stageNumber);
+    }
+
+    public void GameOver()
+    {
+        if (isFinished)
+            return;
+
+        isFinished = true;
+
+        Debug.Log($"{stageNumber} 스테이지 게임오버");
+
+        OnGameOver?.Invoke();
     }
 }
