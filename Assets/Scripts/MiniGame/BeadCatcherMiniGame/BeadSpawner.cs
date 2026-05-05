ddb079a755ddcece85d7777fd5bfd073105a78d3
이현진 <zse846@gmail.com>
2026-05-06 00:42:27 +0900

    중간 저장
    구슬이 생성되고 움직이는 것 구현, 버튼으로 문을 토글 방식으로 열고 닫는 기능 구현

---
+++ Assets/Scripts/MiniGame/BeadCatcherMiniGame/BeadSpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BeadSpawner : MonoBehaviour
+{
+    public GameObject beadPrefab;
+    public Transform spawnPoint;
+    public Transform canvasTransform;
+    public float spawnInterval = 2f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBead", 0f, spawnInterval);
+    }
+
+    void SpawnBead()
+    {
+        GameObject newBead = Instantiate(beadPrefab, spawnPoint.position, Quaternion.identity, canvasTransform);
+
+        char randomLetter = (char)Random.Range('A', 'Z' + 1);
+        newBead.GetComponent<Bead>().SetLetter(randomLetter);
+
+        // Hierarchy 최상단으로 이동
+        newBead.transform.SetAsFirstSibling();
+    }
+}
